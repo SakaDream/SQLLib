3dc33c65ee82cf533dc8507a9ab4537f8f9ce0ed
SakaDream <bahaimt@gmail.com>
2017-02-15 23:12:02 +0700

    Add read / write hibernate.cfg.xml. Create JavaDoc

--- src/com/sakadream/lib/sql/SQL.java
+++ src/com/sakadream/lib/sql/SQL.java
@@ -12,19 +12,30 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- *
+ * SQL Utilities (connect db, select query, update query, echo query)
  * @author Phan Ba Hai
  */
-
 public class SQL {
+
     private static Connection con;
-    
-    public static void createConnection(SQLConfig config){
+
+    /**
+     * Set Connection using SQLConfig object
+     * @param config SQLConfig object
+     */
+    public static void createConnection(SQLConfig config) {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            con = DriverManager.getConnection("jdbc:sqlserver://" + config.getHost() + 
-                    ":" + config.getPort() + 
-                    ";databaseName=" + config.getDbName(), config.getUsername(), config.getPassword());
+            if (config.getUrl() == null & config.getHost() != null
+                    & config.getPort() != null & config.getDbName() != null) {
+                con = DriverManager.getConnection("jdbc:sqlserver://" + config.getHost() 
+                        + ":" + config.getPort() 
+                        + ";databaseName=" + config.getDbName(), 
+                        config.getUsername(), config.getPassword());
+            } else {
+                con = DriverManager.getConnection(config.getUrl(), 
+                        config.getUsername(), config.getPassword());
+            }
         } catch (ClassNotFoundException cnfe) {
             System.out.println("Microsoft JDBC Driver can not be found!");
         } catch (SQLException sqle) {
@@ -34,32 +45,59 @@ public class SQL {
             ex.printStackTrace();
         }
     }
-    
+
+    /**
+     * Set connection using config.json
+     * @throws Exception
+     */
     public static void createConnection() throws Exception {
         SQLConfig config = SQLConfigJson.open();
         createConnection(config);
     }
-    
+
+    /**
+     *
+     * @return java.sql.Connection
+     */
     public static Connection getConnection() {
         return con;
     }
-    
+
+    /**
+     *
+     * @throws SQLException
+     */
     public static void closeConnection() throws SQLException {
         con.close();
     }
-    
+
+    /**
+     * Execute SQL SELECT Query
+     * @param sql SQL Query
+     * @return java.sql.ResultSet
+     * @throws Exception
+     */
     public static ResultSet selectQuery(String sql) throws Exception {
         createConnection();
         Statement stmt = con.createStatement();
         return stmt.executeQuery(sql);
     }
-    
+
+    /**
+     * Execute SQL CREATE / DROP / ALTER / INSERT / UPDATE Query
+     * @param sql SQL Query
+     * @throws Exception
+     */
     public static void updateQuery(String sql) throws Exception {
         createConnection();
         Statement stmt = con.createStatement();
         stmt.executeUpdate(sql);
     }
-    
+
+    /**
+     * Print SQL query into command line
+     * @param sql SQL Query
+     */
     public static void echoQuery(String sql) {
         System.out.println(sql);
     }
--- src/com/sakadream/lib/sql/SQLConfig.java
+++ src/com/sakadream/lib/sql/SQLConfig.java
@@ -6,7 +6,7 @@
 package com.sakadream.lib.sql;
 
 /**
- *
+ * SQLConfig object
  * @author Phan Ba Hai
  */
 public class SQLConfig {
@@ -15,12 +15,26 @@ public class SQLConfig {
     private String dbName;
     private String username = "sa";
     private String password;
+    private String url;
 
+    /**
+     * default values: host = localhost, port = 1433, username = sa
+     * @param dbName Database name
+     * @param password SQL Server password
+     */
     public SQLConfig(String dbName, String password) {
         this.dbName = dbName;
         this.password = password;
     }
 
+    /**
+     *
+     * @param host SQL Server host name
+     * @param port SQL Server port
+     * @param dbName Database name
+     * @param username SQL Server username
+     * @param password SQL Server password
+     */
     public SQLConfig(String host, String port, String dbName, String username, String password) {
         this.host = host;
         this.port = port;
@@ -29,43 +43,111 @@ public class SQLConfig {
         this.password = password;
     }
 
+    /**
+     * SQLConfig object for Hibernate Configuration XML file
+     * @param dbName Database name
+     * @param password SQL Server password
+     * @param url The Connection String (hibernate.hibernate.connection.url property in hibernate.cfg.xml)
+     */
+    public SQLConfig(String dbName, String password, String url) {
+        this.dbName = dbName;
+        this.password = password;
+        this.url = url;
+    }
+
+    /**
+     *
+     * @return String
+     */
     public String getHost() {
         return host;
     }
 
+    /**
+     *
+     * @return String
+     */
     public String getPort() {
         return port;
     }
 
+    /**
+     *
+     * @return String
+     */
     public String getDbName() {
         return dbName;
     }
 
+    /**
+     *
+     * @return String
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     *
+     * @return String
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     *
+     * @return String
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     *
+     * @param host SQL Server host name
+     */
     public void setHost(String host) {
         this.host = host;
     }
 
+    /**
+     *
+     * @param port SQL Server port
+     */
     public void setPort(String port) {
         this.port = port;
     }
 
+    /**
+     *
+     * @param dbName Database name
+     */
     public void setDbName(String dbName) {
         this.dbName = dbName;
     }
 
+    /**
+     *
+     * @param username SQL Server username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     *
+     * @param password SQL Server password
+     */
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    /**
+     *
+     * @param url The Connection String (hibernate.hibernate.connection.url property in hibernate.cfg.xml)
+     */
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
